d3288139b3b09c28431b39e9b7ce0392b1c02c0c
Etienne de Maricourt <edmecomemail@gmail.com>
2026-04-05 23:19:52 +0200

    feat: blink green on GPIO3 (sk6812 LED)

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp_hal::{interrupt::software::SoftwareInterruptControl, timer::timg::TimerGroup};
+use esp_hal::{interrupt::software::SoftwareInterruptControl, timer::timg::TimerGroup, rmt::Rmt};
 use esp_println::println;
+use esp_hal_smartled::{buffer_size_async, SmartLedsAdapterAsync};
+use smart_leds::{SmartLedsWriteAsync, RGB8};
 
 // Provide #[panic_handler]
 use esp_backtrace as _;
+use esp_hal::time::Rate;
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -30,10 +33,22 @@ async fn main(spawner: Spawner) {
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_rtos::start(timg0.timer0, sw_int.software_interrupt0);
 
+    let rmt: Rmt<'_, esp_hal::Async> = {
+        let frequency = Rate::from_mhz(80);
+        Rmt::new(peripherals.RMT, frequency)
+    }.expect("Failed to initialize RMT peripheral").into_async();
+
+    let rmt_channel = rmt.channel0;
+    let mut rmt_buffer = [esp_hal::rmt::PulseCode::default(); buffer_size_async(1)];
+    let mut led = SmartLedsAdapterAsync::new(rmt_channel, peripherals.GPIO3, &mut rmt_buffer);
+
     spawner.spawn(run()).ok();
 
     loop {
         println!("Bing!");
-        Timer::after(Duration::from_millis(5_000)).await;
+        Timer::after(Duration::from_millis(1_000)).await;
+        led.write([RGB8::new(0, 59, 0)]).await.unwrap();
+        Timer::after(Duration::from_millis(1_000)).await;
+        led.write([RGB8::new(0, 0, 0)]).await.unwrap();
     }
 }
