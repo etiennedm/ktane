1a2462b59e5fdb26f6434e055210a9b235018fd7
Etienne de Maricourt <edmecomemail@gmail.com>
2026-04-06 22:59:23 +0200

    feat: blink 4 LEDs for Simon says module

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::{interrupt::software::SoftwareInterruptControl, timer::timg::TimerGroup, rmt::Rmt};
 use esp_println::println;
-use esp_hal_smartled::{buffer_size_async, SmartLedsAdapterAsync};
-use smart_leds::{SmartLedsWriteAsync, RGB8};
+use esp_hal_smartled::{buffer_size_async, buffer_size_rgbw, smart_led_buffer, SmartLedsAdapter, SmartLedsAdapterAsync};
+use smart_leds::{SmartLedsWrite, RGBW, RGBA};
 
 // Provide #[panic_handler]
 use esp_backtrace as _;
@@ -33,22 +33,37 @@ async fn main(spawner: Spawner) {
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_rtos::start(timg0.timer0, sw_int.software_interrupt0);
 
-    let rmt: Rmt<'_, esp_hal::Async> = {
+    const LED_COUNT: usize = 4;
+    let rmt: Rmt<'_, esp_hal::Blocking> = {
         let frequency = Rate::from_mhz(80);
         Rmt::new(peripherals.RMT, frequency)
-    }.expect("Failed to initialize RMT peripheral").into_async();
+    }.expect("Failed to initialize RMT peripheral");
 
     let rmt_channel = rmt.channel0;
-    let mut rmt_buffer = [esp_hal::rmt::PulseCode::default(); buffer_size_async(1)];
-    let mut led = SmartLedsAdapterAsync::new(rmt_channel, peripherals.GPIO3, &mut rmt_buffer);
+    let mut rmt_buffer = [esp_hal::rmt::PulseCode::default(); buffer_size_rgbw(LED_COUNT)];
+    let mut led: SmartLedsAdapter<'_, { buffer_size_rgbw(LED_COUNT) }, RGBA<u8>> = SmartLedsAdapter::new_with_color(rmt_channel, peripherals.GPIO3, &mut rmt_buffer);
 
     spawner.spawn(run()).ok();
 
     loop {
         println!("Bing!");
         Timer::after(Duration::from_millis(1_000)).await;
-        led.write([RGB8::new(0, 59, 0)]).await.unwrap();
+        led.write(
+            [
+                RGBA::new(60, 0, 0, 0),
+                RGBA::new(0, 60, 0, 0),
+                RGBA::new(0, 0, 60, 0),
+                RGBA::new(60, 60, 0, 0),
+            ]
+        ).unwrap();
         Timer::after(Duration::from_millis(1_000)).await;
-        led.write([RGB8::new(0, 0, 0)]).await.unwrap();
+        led.write(
+            [
+                RGBA::new(0, 0, 0, 0),
+                RGBA::new(0, 0, 0, 0),
+                RGBA::new(0, 0, 0, 0),
+                RGBA::new(0, 0, 0, 0),
+            ]
+        ).unwrap();
     }
 }
