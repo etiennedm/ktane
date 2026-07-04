dcc1286f63c884a12afa27d3f6b35d6b95dffb7e
Etienne de Maricourt <edmecomemail@gmail.com>
2026-07-04 18:22:31 +0200

    timer: add I2C based driver for the tm1637 7-segment display

--- timer/src/main.rs
+++ timer/src/main.rs
@@ -1,12 +1,17 @@
 #![no_std]
 #![no_main]
 
+mod tm1637;
+
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::{
-    interrupt::software::SoftwareInterruptControl, timer::timg::TimerGroup,
+    interrupt::software::SoftwareInterruptControl, timer::timg::TimerGroup, time::Rate,
 };
+use esp_hal::i2c::master::{Config, I2c};
 use log::info;
+
+use tm1637::{BrightnessLevel, SevenSegmentChar, TM1637};
 
 // Provide #[panic_handler]
 use esp_backtrace as _;
@@ -24,11 +29,48 @@ async fn main(_spawner: Spawner) {
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_rtos::start(timg0.timer0, sw_int.software_interrupt0);
 
+    let i2c_config = Config::default().with_frequency(Rate::from_khz(100));
+    let i2c0 = I2c::new(peripherals.I2C0, i2c_config)
+        .unwrap()
+        .into_async()
+        .with_sda(peripherals.GPIO5)
+        .with_scl(peripherals.GPIO4);
+
+    let mut tm = TM1637::new(i2c0);
+
     info!("ESP RTOS scheduler started");
 
     loop {
-        info!("Running...");
+        tm.with_digits([SevenSegmentChar::Digit0; 4]).with_display_on().refresh().await;
+        for (i, level) in [BrightnessLevel::Level0, BrightnessLevel::Level1, BrightnessLevel::Level2, BrightnessLevel::Level3, BrightnessLevel::Level4, BrightnessLevel::Level5, BrightnessLevel::Level6, BrightnessLevel::Level7].iter().enumerate() {
+            tm.with_brightness_level(*level).refresh().await;
+            Timer::after(Duration::from_millis(250)).await;
+        }
+
+        for i in 0..10{
+            tm.with_colon_on(i%2 == 0).refresh().await;
+            Timer::after(Duration::from_millis(250)).await;
+        }
+
+        for i in 0..10000 {
+            let mut digits = [SevenSegmentChar::Blank; 4];
+            for (index, digit) in digits.iter_mut().rev().enumerate() {
+                *digit = (((i / (10_i32.pow(index as u32))) % 10) as u8).into()
+            }
+            for digit in digits.iter_mut() {
+                if *digit == SevenSegmentChar::Digit0 {
+                    *digit = SevenSegmentChar::Blank;
+                } else {
+                    break;
+                }
+            }
+            tm.with_digits(digits).refresh().await;
+            Timer::after(Duration::from_millis(10)).await;
+        }
+
+        tm.with_display_off().refresh().await;
         Timer::after(Duration::from_secs(1)).await;
+        info!("Tick");
     }
 }
 
---
+++ timer/src/tm1637.rs
@@ -0,0 +1,151 @@
+use embedded_hal_async::i2c::I2c;
+use log::info;
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub enum BrightnessLevel {
+    // Flip bits since LSB first is expected by the TM1637 but I2C is sending MSB first
+    Level0 = 0, // 0b000 -> 0b000
+    Level1 = 4, // 0b001 -> 0b100
+    Level2 = 2, // 0b010 -> 0b010
+    Level3 = 6, // 0b011 -> 0b110
+    Level4 = 1, // 0b100 -> 0b001
+    Level5 = 5, // 0b101 -> 0b101
+    Level6 = 3, // 0b110 -> 0b011
+    Level7 = 7, // 0b111 -> 0b111
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub enum SevenSegmentChar {
+    Digit0,
+    Digit1,
+    Digit2,
+    Digit3,
+    Digit4,
+    Digit5,
+    Digit6,
+    Digit7,
+    Digit8,
+    Digit9,
+    Blank,
+}
+
+impl From<u8> for SevenSegmentChar {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => SevenSegmentChar::Digit0,
+            1 => SevenSegmentChar::Digit1,
+            2 => SevenSegmentChar::Digit2,
+            3 => SevenSegmentChar::Digit3,
+            4 => SevenSegmentChar::Digit4,
+            5 => SevenSegmentChar::Digit5,
+            6 => SevenSegmentChar::Digit6,
+            7 => SevenSegmentChar::Digit7,
+            8 => SevenSegmentChar::Digit8,
+            9 => SevenSegmentChar::Digit9,
+            _ => SevenSegmentChar::Blank
+        }
+    }
+}
+
+impl From<SevenSegmentChar> for u8 {
+    fn from(value: SevenSegmentChar) -> Self {
+        match value {
+            SevenSegmentChar::Digit0 => 0b1111_1100,
+            SevenSegmentChar::Digit1 => 0b0110_0000,
+            SevenSegmentChar::Digit2 => 0b1101_1010,
+            SevenSegmentChar::Digit3 => 0b1111_0010,
+            SevenSegmentChar::Digit4 => 0b0110_0110,
+            SevenSegmentChar::Digit5 => 0b1011_0110,
+            SevenSegmentChar::Digit6 => 0b1011_1110,
+            SevenSegmentChar::Digit7 => 0b1110_0000,
+            SevenSegmentChar::Digit8 => 0b1111_1110,
+            SevenSegmentChar::Digit9 => 0b1111_0110,
+            SevenSegmentChar::Blank => 0b0000_0000,
+        }
+    }
+}
+
+pub struct TM1637<I2C> {
+    i2c: I2C,
+    brightness_level: BrightnessLevel,
+    display_on: bool,
+    colon_on: bool,
+    digits: [SevenSegmentChar; 4],
+}
+
+impl<I2C> TM1637<I2C>
+where
+    I2C: I2c<Error: core::fmt::Display>,
+{
+    pub fn new(i2c: I2C) -> Self {
+        Self {
+            i2c,
+            brightness_level: BrightnessLevel::Level0,
+            display_on: false,
+            colon_on: false,
+            digits: [SevenSegmentChar::Blank; 4],
+        }
+    }
+
+    pub fn with_brightness_level(&mut self, brightness_level: BrightnessLevel) -> &mut Self {
+        self.brightness_level = brightness_level;
+        self
+    }
+
+    pub fn with_display_on(&mut self) -> &mut Self {
+        self.display_on = true;
+        self
+    }
+
+    pub fn with_display_off(&mut self) -> &mut Self {
+        self.display_on = false;
+        self
+    }
+
+    pub fn with_colon_on(&mut self, colon_on: bool) -> &mut Self {
+        self.colon_on = colon_on;
+        self
+    }
+
+    pub fn with_digits(&mut self, digits: [SevenSegmentChar; 4]) -> &mut Self {
+        self.digits = digits;
+        self
+    }
+
+    pub async fn refresh(&mut self) {
+        match self.i2c.write(0x01, &[]).await {
+            Err(e) => info!("I2C write error for command: {}", e),
+            _ => {}
+        };
+
+        // Invalid command (LSB = 00) followed by data for C0H-C3H, adding a dummy 0xFF last makes it
+        // somehow loop around to accept the next update.
+        let mut digit_data = [0xFF; 5];
+        for (i, digit) in self.digits.iter().enumerate() {
+            digit_data[i] = (*digit).into();
+        }
+
+        if self.colon_on {
+            digit_data[1] += 1;
+        }
+
+        match self
+            .i2c
+            .write(
+                0x00, /* 0xFF on bus */
+                &digit_data,
+            )
+            .await
+        {
+            Err(e) => info!("I2C write error for command: {}", e),
+            _ => {}
+        };
+
+        // Send display control command to enable display and set brightness level
+        let display_cmd = (self.display_on as u8) << 3 | (self.brightness_level as u8) << 4;
+        match self.i2c.read(display_cmd, &mut [0; 1]).await {
+            Err(e) => info!("I2C write error for display: {}", e),
+            _ => {}
+        };
+    }
+}
